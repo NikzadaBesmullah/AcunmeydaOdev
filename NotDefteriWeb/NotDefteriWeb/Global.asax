38e983b8978607610d0dbd2544d370b99753d0de
Besmullah Nikzada <besmullahnikzada786@gmail.com>
2025-03-25 18:53:38 +0300

    css
    css dahil edildi

--- NotDefteriWeb/NotDefteriWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NotDefteriWeb.MvcApplication" Language="C#" %>
---
+++ NotDefteriWebsite/NotDefteriWebsite/Controllers/NoteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotDefteriWebsite.Models;
+
+namespace NotDefteriWebsite.Controllers
+{
+    public class NoteController : Controller
+    {
+
+        public ActionResult index()
+        {
+            var not = new List<Note>()
+            {
+            new Note() { Noteid = 2, NoteTitle = "İnglizce", NoteContent = "İnglizce Kursunu bitir" , NoteAddTime= new DateTime(2025,03,14), NoteCategory="Dil" } ,
+            new Note() { Noteid = 3, NoteTitle = "Acunmedya", NoteContent = "Asp net dersini tekrar et" , NoteAddTime= new DateTime(2025,1,05),NoteCategory="Staj"} ,
+            new Note() { Noteid = 4, NoteTitle = "Ağ Temelleri", NoteContent = "Slaytı Bitir" , NoteAddTime=DateTime.Now,NoteCategory="okul"} ,
+
+            };
+            return View(not);
+        }
+        
+        }
+    }
+
